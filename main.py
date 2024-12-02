a6e444141239036fd1ce05824e7ec1e7cb63fcdc
Apex <ilind663@gmail.com>
2024-12-02 23:35:29 +0300

    second exercise of homework

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import sqlite3
 import sys
 
 from PyQt6 import uic
 from PyQt6.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt6.QtWidgets import QTableView, QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.addButton.clicked.connect(self.add)
         self.initUI()
 
     def initUI(self):
@@ -16,15 +18,24 @@ class Example(QMainWindow):
         db.setDatabaseName('coffee.sqlite')
         db.open()
 
-        view = QTableView(self)
-
         model = QSqlTableModel(self, db)
         model.setTable('coffe_name')
         model.select()
 
-        view.setModel(model)
-        view.move(90, 230)
-        view.resize(617, 315)
+        self.view.setModel(model)
+        self.view.move(90, 230)
+        self.view.resize(617, 315)
+
+    def add(self):
+        sqlite_connection = sqlite3.connect('coffee.sqlite')
+        cursor = sqlite_connection.cursor()
+        cursor.execute('INSERT INTO coffe_name(name_of_sort) VALUES ("")')
+        sqlite_connection.commit()
+        cursor.close()
+        try:
+            self.initUI()
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
